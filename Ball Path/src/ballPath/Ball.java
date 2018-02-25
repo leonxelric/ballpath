33c86ccb96fcd3789796090ca70a11e4668979b3
LEON-LAPTOP\leonx <lxie21@mlschools.org>
2018-02-25 05:07:57 -0500

    ball basics

--- Ball Path/src/ballPath/Ball.java
+++ Ball Path/src/ballPath/Ball.java
@@ -1,5 +1,86 @@
 package ballPath;
 
-public class Ball {
+//import java.io.*;
+import java.util.*;
+import processing.core.PApplet;
 
+public class Ball 
+{
+	private ArrayList<Integer> instructions;
+	private PApplet parent;
+	private double posX;
+	private double posY;
+	
+	public Ball(PApplet p, int startX, int startY)
+	{
+		parent = p;
+		posX = startX;
+		posY = startY;
+		instructions = new ArrayList<Integer>();
+	}
+	
+	public void moveBall()
+	{
+		for(int i = 0; i < instructions.size(); i++)
+		{
+			if(instructions.get(i) == 0)
+				posY -= 8;
+			else if(instructions.get(i) == 1)
+				posX += 8;
+			else if(instructions.get(i) == 2)
+				posY += 8;
+			else if(instructions.get(i) == 3)
+				posX -= 8;
+			parent.background(0);
+			drawBall();
+		}
+	}
+	
+	public void changeVelocity(int newXMove, int newYMove)
+	{
+		
+	}
+	
+	public void drawBall()
+	{
+		parent.fill(50, 200, 0);
+		parent.ellipse((float)posX, (float)posY, 50, 50);
+	}
+	
+	public void addMove(int move)
+	{
+		
+		instructions.add(move);
+	}
+	
+	public int getDiffMove()
+	{
+		int move = (int)parent.random(0, 4);
+		if(instructions.size() < 1)
+			return move;
+		while(move == instructions.get(instructions.size() - 1))
+			move = (int)parent.random(0, 4);
+		return move;
+	}
+	
+	public void removeMove()
+	{
+		instructions.remove(instructions.size() - 1);
+	}
+	
+	public boolean inBounds(int rectX, int rectY, int size)
+	{
+		return (posX > rectX && posX < rectX + size) && ((posY > rectY && posY < rectY + size));
+	}
+	
+	public void setPos(double pX, double pY)
+	{
+		posX = pX;
+		posY = pY;
+	}
+	
+	public ArrayList<Integer> getInstruct()
+	{
+		return instructions;
+	}
 }
