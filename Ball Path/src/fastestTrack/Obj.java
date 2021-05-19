9acbc902d2e1991520e58c3deb6dcf05a51d707f
Quinn's Laptop <Quinn's Laptop@XPS.lan>
2021-05-19 03:29:15 -0400

    bruh ig

--- Ball Path/src/fastestTrack/Obj.java
+++ Ball Path/src/fastestTrack/Obj.java
@@ -1,30 +1,102 @@
 package fastestTrack;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
 
 public class Obj
 {
 	PApplet p;
-	int x;
-	int y;
+	final double scale = (double) .005;
+	double x;
+	double y;
+	double xvel;
+	double yvel;
+	double theta;
 	int dia;
 	
-	public Obj(int x, int y, int dia, PApplet parent)
+	public Obj(double x, double y, double inixvel, double iniyvel, int dia, double iniTheta, PApplet parent)
 	{
 		this.x = x;
 		this.y = y;
+		xvel = inixvel;
+		yvel = iniyvel;
 		this.dia = dia;
+		theta = iniTheta;
 		p = parent;
 	}
 	
 	public void drawSelf()
 	{
-		p.ellipse(x, y, dia, dia);
+		p.ellipse((int) x, (int) y, dia, dia);
 	}
 	
-	public void move(float x, float y)
+	public void setVels(double xmov, double ymov)
+	{
+		xvel = xmov;
+		yvel = ymov;
+	}
+	
+	public void move()
+	{
+		x += xvel;
+		y += yvel;
+	}
+	
+	public void move(double x, double y)
 	{
 		this.x += x;
 		this.y += y;
 	}
+	
+	public void acc()
+	{
+		
+		yvel += 9.81 * scale;
+		xvel += 9.81 * Math.sin(theta) * scale;
+		yvel -= 9.81 * Math.cos(theta) * scale;
+		move();
+		
+//		yvel += 9.81 * (1 - Math.cos(theta)) * .005;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Ball Path/src/fastestTrack/Tracker.java
+++ Ball Path/src/fastestTrack/Tracker.java
@@ -2,7 +2,7 @@ package fastestTrack;
 
 import processing.core.PApplet;
 
-public abstract class Tracker extends PApplet
+public class Tracker extends PApplet
 {
 	
 	Obj obj1;
@@ -19,12 +19,22 @@ public abstract class Tracker extends PApplet
 	
 	public void setup()
 	{
-		obj1 = new Obj(50, 50, 10, this);
+		noStroke();
+		obj1 = new Obj(50, height/5, 0, 0, 10, (double) Math.toRadians(90), this);
 	}
 	
 	public void draw()
 	{
-		obj1.move(width/100, height/100);
+		background(0);
+		
+		obj1.acc();
+		obj1.drawSelf();
+		
+//		obj1.theta -= .007;
+		
+		System.out.println(Math.toDegrees(obj1.theta));
+		
+//		System.out.println(Math.asin(obj1.xvel) + Math.acos(obj1.yvel));
 	}
 	
 	
